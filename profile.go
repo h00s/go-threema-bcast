ea54b0f9b2bee5fd073e5a4e4acecac0273b665a
Krunoslav Husak <kruno@binel.hr>
2021-02-27 08:01:52 +0100

    Added profile and profile method

---
+++ profile.go
@@ -0,0 +1,24 @@
+package go_threema_bcast
+
+import (
+	"encoding/json"
+)
+
+type Profile struct {
+	links Links
+}
+
+func (c *Client) GetProfile() (Profile, error) {
+	var p Profile
+
+	body, err := c.Get("/")
+	if err != nil {
+		return p, err
+	}
+
+	if err := json.Unmarshal(body, &p); err != nil {
+		return p, err
+	}
+
+	return p, nil
+}
