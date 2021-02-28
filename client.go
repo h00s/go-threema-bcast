c5ec9210783c3f0beb38c97f90a03dfe52fa27c4
Krunoslav Husak <kruno@binel.hr>
2021-02-28 15:27:38 +0100

    Reindent with spaces

--- client.go
+++ client.go
@@ -1,59 +1,59 @@
 package go_threema_bcast
 
 import (
-	"fmt"
-	"io"
-	"net/http"
+  "fmt"
+  "io"
+  "net/http"
 )
 
 // BaseURL is the default base URL for the Threema Broadcast API
 const BaseURL = "https://broadcast.threema.ch/api/v1"
 
 type Client struct {
-	HTTPClient *http.Client
-	BaseURL    string
-	APIKey     string
+  HTTPClient *http.Client
+  BaseURL    string
+  APIKey     string
 }
 
 type Links struct {
-	Links []Link `json:"_links"`
+  Links []Link `json:"_links"`
 }
 
 type Link struct {
-	Ref  string `json:"ref"`
-	Link string `json:"link"`
+  Ref  string `json:"ref"`
+  Link string `json:"link"`
 }
 
 // NewClient constructs a client using http.DefaultClient and the default
 // base URL. The returned client is ready for use.
 func NewClient(APIKey string) *Client {
-	return &Client{
-		HTTPClient: http.DefaultClient,
-		BaseURL:    BaseURL,
-		APIKey:     APIKey,
-	}
+  return &Client{
+    HTTPClient: http.DefaultClient,
+    BaseURL:    BaseURL,
+    APIKey:     APIKey,
+  }
 }
 
 func (c *Client) Get(path string) ([]byte, error) {
-	req, err := http.NewRequest("GET", c.BaseURL+path, nil)
-	if err != nil {
-		return nil, fmt.Errorf("failed to build request: %v", err)
-	}
-	req.Header.Add("X-Api-Key", c.APIKey)
+  req, err := http.NewRequest("GET", c.BaseURL+path, nil)
+  if err != nil {
+    return nil, fmt.Errorf("failed to build request: %v", err)
+  }
+  req.Header.Add("X-Api-Key", c.APIKey)
 
-	resp, err := c.HTTPClient.Do(req)
-	if err != nil {
-		return nil, fmt.Errorf("failed to do request: %v", err)
-	}
-	defer resp.Body.Close()
+  resp, err := c.HTTPClient.Do(req)
+  if err != nil {
+    return nil, fmt.Errorf("failed to do request: %v", err)
+  }
+  defer resp.Body.Close()
 
-	if resp.StatusCode != 200 {
-		return nil, fmt.Errorf("returned status code: %v", resp.StatusCode)
-	}
+  if resp.StatusCode != 200 {
+    return nil, fmt.Errorf("returned status code: %v", resp.StatusCode)
+  }
 
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return nil, fmt.Errorf("failed to read body: %v", err)
-	}
-	return body, nil
+  body, err := io.ReadAll(resp.Body)
+  if err != nil {
+    return nil, fmt.Errorf("failed to read body: %v", err)
+  }
+  return body, nil
 }
--- profile.go
+++ profile.go
@@ -1,24 +1,24 @@
 package go_threema_bcast
 
 import (
-	"encoding/json"
+  "encoding/json"
 )
 
 type Profile struct {
-	links Links
+  links Links
 }
 
 func (c *Client) GetProfile() (Profile, error) {
-	var p Profile
+  var p Profile
 
-	body, err := c.Get("/")
-	if err != nil {
-		return p, err
-	}
+  body, err := c.Get("/")
+  if err != nil {
+    return p, err
+  }
 
-	if err := json.Unmarshal(body, &p); err != nil {
-		return p, err
-	}
+  if err := json.Unmarshal(body, &p); err != nil {
+    return p, err
+  }
 
-	return p, nil
+  return p, nil
 }
