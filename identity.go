410509c188bcc2ab786a0cd28cead2a68a393bde
Krunoslav Husak <kruno@binel.hr>
2021-03-06 12:15:11 +0100

    Added unmarshal method on Identities struct

--- identity.go
+++ identity.go
@@ -1,34 +1,38 @@
 package go_threema_bcast
 
 import (
-  "encoding/json"
+	"encoding/json"
 )
 
 type Identities struct {
-  Links []Link `json:"_links"`
-  Identities []Identity `json:"identities"`
+	Links      []Link     `json:"_links"`
+	Identities []Identity `json:"identities"`
 }
 
 type Identity struct {
-  Links []Link `json:"_links"`
-  Id string `json:"id"`
-  ThreemaId string `json:"threemaId"`
-  ValidUntil DateTime `json:"validUntil"`
-  Type string `json:"type"`
-  RecipientLimit int `json:"recipientLimit"`
+	Links          []Link   `json:"_links"`
+	Id             string   `json:"id"`
+	ThreemaId      string   `json:"threemaId"`
+	ValidUntil     DateTime `json:"validUntil"`
+	Type           string   `json:"type"`
+	RecipientLimit int      `json:"recipientLimit"`
 }
 
-func (c *Client) GetIdentities() (Identities, error) {
-  var i Identities
+func (c *Client) GetIdentities() (*Identities, error) {
+	i := &Identities{}
 
-  body, err := c.Get("/identities")
-  if err != nil {
-    return i, err
-  }
+	body, err := c.Get("/identities")
+	if err != nil {
+		return i, err
+	}
 
-  if err := json.Unmarshal(body, &i); err != nil {
-    return i, err
-  }
+	return i.Unmarshal(body)
+}
 
-  return i, nil
+func (p *Identities) Unmarshal(body []byte) (*Identities, error) {
+	if err := json.Unmarshal(body, &p); err != nil {
+		return p, err
+	}
+
+	return p, nil
 }
