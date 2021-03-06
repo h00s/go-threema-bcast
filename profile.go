299faa6b883a90f685f8e62175ef98fb8f13fc93
Krunoslav Husak <kruno@binel.hr>
2021-03-06 12:10:45 +0100

    Added unmarshal method on Profile struct

--- profile.go
+++ profile.go
@@ -1,24 +1,28 @@
 package go_threema_bcast
 
 import (
-  "encoding/json"
+	"encoding/json"
 )
 
 type Profile struct {
-  Links []Link `json:"_links"`
+	Links []Link `json:"_links"`
 }
 
-func (c *Client) GetProfile() (Profile, error) {
-  var p Profile
+func (c *Client) GetProfile() (*Profile, error) {
+	p := &Profile{}
 
-  body, err := c.Get("/")
-  if err != nil {
-    return p, err
-  }
+	body, err := c.Get("/")
+	if err != nil {
+		return p, err
+	}
 
-  if err := json.Unmarshal(body, &p); err != nil {
-    return p, err
-  }
+	return p.Unmarshal(body)
+}
 
-  return p, nil
+func (p *Profile) Unmarshal(body []byte) (*Profile, error) {
+	if err := json.Unmarshal(body, &p); err != nil {
+		return p, err
+	}
+
+	return p, nil
 }
